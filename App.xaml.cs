e624c747757684edd92c0e6fca3551963dcf0190
gm-cal <gm.cal.1988@gmail.com>
2025-07-20 04:40:42 +0900

    Fix namespace for App class so startup runs

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,13 +2,15 @@ using System.Windows;
 using Services;
 using ViewModels;
 
-public partial class App : Application{
-    private readonly IFileService fileService = new FileService();
-    private readonly IJsonService jsonService = new JsonService();
+namespace JsonEditor2{
+    public partial class App : Application{
+        private readonly IFileService fileService = new FileService();
+        private readonly IJsonService jsonService = new JsonService();
 
-    protected override void OnStartup(StartupEventArgs e){
-        base.OnStartup(e);
-        MainWindow window = new MainWindow(fileService, jsonService);
-        window.Show();
+        protected override void OnStartup(StartupEventArgs e){
+            base.OnStartup(e);
+            MainWindow window = new MainWindow(fileService, jsonService);
+            window.Show();
+        }
     }
 }
