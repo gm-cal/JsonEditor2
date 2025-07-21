d2d578dae35dd2c81a17caf120d434df4e5d553d
gm-cal <gm.cal.1988@gmail.com>
2025-07-22 07:58:15 +0900

    Fix namespace and icon

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using Services;
 using ViewModels;
 
+namespace JsonEditor2{
 public partial class App : Application{
     private readonly IFileService fileService = new FileService();
     private readonly IJsonService jsonService = new JsonService();
@@ -12,4 +13,5 @@ public partial class App : Application{
         MainWindow window = new MainWindow(fileService, jsonService, textService);
         window.Show();
     }
+}
 }
